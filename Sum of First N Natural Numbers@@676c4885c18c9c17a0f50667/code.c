c50092ef24579a5496478667f3465b92f9e7996b
Ujjwaldeepkaur <ujjwaldeepkaur@gmail.com>
2025-03-28 19:58:35 +0530

    Solution to Sum of First N Natural Numbers

--- Sum of First N Natural Numbers@@676c4885c18c9c17a0f50667/code.c
+++ Sum of First N Natural Numbers@@676c4885c18c9c17a0f50667/code.c
@@ -1,47 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,i,count1=0,count2;
-    printf("Enter 2 number:");
-    scanf("%d%d",&a,&b);
-    for (i=2;i<=a/2;i++)
-    {
-        if (a%i==0)
-        {
-            count1 =1;
-            break;
+    for(int i=1;i<=100;i++){
+        if(i%2==0){
+            printf("%d",i);
         }
-
-        else
-        {
-            count1 =0;
-        }
-
     }
-
-     for (i=2;i<=b/2;i++)
-    {
-        if (a%i==0)
-        {
-            count2 =1;
-            break;
-        }
-
-        else
-        {
-            count2 =0;
-        }
-
-    }
-
-    if (count1==0 && count2==0 && (a-b==2 || b-a==2))
-    {
-        printf("Twin Prime\n");
-    }
-
-    else
-    {
-        printf("Not Twin Prime\n");
-    }
-return 0;
+    return 0;
 }
