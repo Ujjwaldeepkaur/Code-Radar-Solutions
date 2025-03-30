641cff21886068589b6e5c49301e252c0bd11b85
Ujjwaldeepkaur <ujjwaldeepkaur@gmail.com>
2025-03-30 18:55:40 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int fibonacci(){
+    int n;
+    int a=0,b=1,sum;
+    scanf("%d",&n);
+    for(int i=1,i<=n,i++)
+    {
+        printf("%d",a);
+        sum=a+b
+        a=b
+        b=sum
+    }  
+}
+int main()
+{
+    fibonacci():
+    return 0;
+}
